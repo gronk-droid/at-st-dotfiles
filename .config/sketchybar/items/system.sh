aa91d5facd5d4f1463132fc65ebe58541dd7d255
gronk-droid <gronk.droid@gmail.com>
2025-07-08 00:36:28 -0400

    update padding of icon and label

--- .config/sketchybar/items/system.sh
+++ .config/sketchybar/items/system.sh
@@ -10,7 +10,7 @@
 # fi
 
 BATT_SCRIPT="battery.sh"
-BATT_ICON_SIZE="$FONT:Regular:13.0"
+BATT_ICON_SIZE="$FONT:Regular:42.0"
 
 
 
@@ -19,8 +19,10 @@ sketchybar  --add       item              battery right                         
                                           script="$PLUGIN_DIR/$BATT_SCRIPT"             \
                                           icon.font="$BATT_ICON_SIZE"                   \
                                           icon.y_offset=1.5                             \
-                                          icon.padding_right=-1                         \
+                                          icon.padding_right=2                          \
                                           label.font="$FONT:Bold Italic:14.0"           \
+                                          padding_right=10                              \
+                                          padding_left=10                               \
             --subscribe battery           system_woke                                   \
             --subscribe battery           space_change                                  \
                                                                                         \

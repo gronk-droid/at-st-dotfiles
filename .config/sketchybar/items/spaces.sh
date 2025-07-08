a7f5de0955d2e3b6a16ca441a61d716e3b7866d4
gronk-droid <gronk.droid@gmail.com>
2025-07-08 00:25:17 -0400

    bracket spaces and pad background

--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -10,53 +10,71 @@ sketchybar  --add       space               space_template left                 
                                             script="$PLUGIN_DIR/space.sh"       \
                                             click_script="$SPACE_CLICK_SCRIPT"  \
                                                                                 \
-            --clone     one                 space_template                      \
-            --set       one                 associated_space=1                  \
+                                                                                \
+            --clone     spaces.one          space_template                      \
+            --set       spaces.one          associated_space=1                  \
                                             icon=一                             \
                                             drawing=on                          \
+                                            background.padding_left=10          \
                                                                                 \
-            --clone     two                 space_template                      \
-            --set       two                 associated_space=2                  \
+            --clone     spaces.two          space_template                      \
+            --set       spaces.two          associated_space=2                  \
                                             icon=二                             \
                                             drawing=on                          \
                                                                                 \
-            --clone     three               space_template                      \
-            --set       three               associated_space=3                  \
+            --clone     spaces.three        space_template                      \
+            --set       spaces.three        associated_space=3                  \
                                             icon=三                             \
                                             drawing=on                          \
                                                                                 \
-            --clone     four                space_template                      \
-            --set       four                associated_space=4                  \
+            --clone     spaces.four         space_template                      \
+            --set       spaces.four         associated_space=4                  \
                                             icon=四                             \
                                             drawing=on                          \
                                                                                 \
-            --clone     five                space_template                      \
-            --set       five                associated_space=5                  \
+            --clone     spaces.five         space_template                      \
+            --set       spaces.five         associated_space=5                  \
                                             icon=五                             \
                                             drawing=on                          \
                                                                                 \
-            --clone     six                 space_template                      \
-            --set       six                 associated_space=6                  \
+            --clone     spaces.six          space_template                      \
+            --set       spaces.six          associated_space=6                  \
                                             icon=六                             \
                                             drawing=on                          \
                                                                                 \
-            --clone     seven               space_template                      \
-            --set       seven               associated_space=7                  \
+            --clone     spaces.seven        space_template                      \
+            --set       spaces.seven        associated_space=7                  \
                                             icon=七                             \
                                             drawing=on                          \
                                                                                 \
-            --clone     eight               space_template                      \
-            --set       eight               associated_space=8                  \
+            --clone     spaces.eight        space_template                      \
+            --set       spaces.eight        associated_space=8                  \
                                             icon=八                             \
                                             drawing=on                          \
                                                                                 \
-            --clone     nine                space_template                      \
-            --set       nine                associated_space=9                  \
+            --clone     spaces.nine         space_template                      \
+            --set       spaces.nine         associated_space=9                  \
                                             icon=九                             \
                                             drawing=on                          \
                                                                                 \
-            --clone     ten                 space_template                      \
-            --set       ten                 associated_space=10                 \
+            --clone     spaces.ten          space_template                      \
+            --set       spaces.ten          associated_space=10                 \
                                             icon=〇                             \
                                             drawing=on                          \
+                                            background.padding_right=10         \
+            --add       bracket             spaces                              \
+                                            spaces.one                          \
+                                            spaces.two                          \
+                                            spaces.three                        \
+                                            spaces.four                         \
+                                            spaces.five                         \
+                                            spaces.six                          \
+                                            spaces.seven                        \
+                                            spaces.eight                        \
+                                            spaces.nine                         \
+                                            spaces.ten                          \
+            --set       spaces              background.drawing=on               \
+                                            padding_left=20                     \
+                                            padding_right=20                    \
+
                                           

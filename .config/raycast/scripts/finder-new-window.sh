ccd883bab3ce09784019b0fa289b02cf52f57c6c
gronk-droid <gronk.droid@gmail.com>
2025-07-09 09:52:25 -0400

    new opens for finder and ghostty windows

---
+++ .config/raycast/scripts/finder-new-window.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title finder-new-window
+# @raycast.mode silent
+
+# Documentation:
+# @raycast.author gronk-droid
+# @raycast.authorURL github.com/gronk-droid
+
+open -n ~
---
+++ .config/raycast/scripts/ghostty-new-term.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title ghostty-new-term
+# @raycast.mode silent
+
+# Documentation:
+# @raycast.author gronk-droid
+# @raycast.authorURL github.com/gronk-droid
+
+open -na Ghostty
